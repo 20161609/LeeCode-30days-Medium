f51a44a0fe49dafde8563b52c35e82d12df17b20
Hojun An <pockguy96@gmail.com>
2025-11-18 11:41:45 -0500

    feat: add day03-problem 1046(to practice heapq)'s solution.

--- day03_practice_heaq/solution_1046.py
+++ day03_practice_heaq/solution_1046.py
@@ -1,2 +1,27 @@
 # https://leetcode.com/problems/last-stone-weight/?utm_source=chatgpt.com
 
+import heapq
+from heapq import heappush, heapify, heappop
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        n = len(stones)
+        for i in range(n):
+            stones[i] *= -1
+            
+        heapify(stones)
+        while len(stones)>1:
+            heappush(stones, heappop(stones)-heappop(stones))
+
+        return -stones.pop()
+    
+# Runtime
+# 0
+# ms
+# Beats
+# 100.00%
+# Analyze Complexity
+# Memory
+# 17.86
+# MB
+# Beats
+# 31.14%
