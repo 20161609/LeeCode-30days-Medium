fedc782859d66421bca435c1969b1646273a82b4
Hojun An <pockguy96@gmail.com>
2025-11-26 11:37:49 -0500

    feat: add problem day03's practice heapq 506's solution.

--- day03_practice_heaq/solution_506.py
+++ day03_practice_heaq/solution_506.py
@@ -1,2 +1,16 @@
 # https://leetcode.com/problems/relative-ranks/description/
 
+import heapq
+
+class Solution:
+    def findRelativeRanks(self, score: List[int]) -> List[str]:
+        n = len(score)
+        answer = [0] * n
+        box = [[v, i]for i, v in enumerate(score)]
+        heapq.heapify(box)
+        medal = {1:'Gold Medal', 2: 'Silver Medal', 3: 'Bronze Medal'}
+        while box:
+            _, i = heapq.heappop(box)
+            answer[i] = medal[n] if n in medal else f"{n}"
+            n -= 1
+        return answer
